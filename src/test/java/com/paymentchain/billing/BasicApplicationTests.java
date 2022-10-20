4281ff07688eb0373ec1ca3bc2ce3de0164eed65
Herbert Sal <betosach@gmail.com>
2022-10-19 19:39:52 -0500

    Se modifica test

--- src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,9 +1,11 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.postgresql.transaction.messages_bg;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
