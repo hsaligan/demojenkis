556a895ee841ad7f824bf2a8f18ac969d50d8045
Herbert Sal <betosach@gmail.com>
2022-10-19 19:35:39 -0500

    Se modifica test

--- src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message = "Default message"
+		String message = "Default message";
 		Assert.assertNotNull(message);
 	}
 
